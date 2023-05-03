79c064cc7a1a76865440c734db3f3324e777681a
shivam1207 <37778200+shivam1207@users.noreply.github.com>
2023-05-03 11:35:44 +0530

    Update HelloWorldController.java

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping("/hello")
     public String sendGreetings() {
-        return "Hello, World! this is testing with jenkins 2 with docker test again 2";
+        return "Hello, World! this is testing with jenkins 2 with docker test again 10";
     }
 }
